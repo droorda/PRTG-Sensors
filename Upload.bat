636bed7734202c019b666c3575a815389874de1f
Doug Roorda <droorda@iconicgroup.com>
2024-01-19 11:04:04 -0500

    Update Upload.bat

--- Upload.bat
+++ Upload.bat
@@ -1,6 +1,6 @@
 @echo off
 
-Copy /y "\\tsclient\D\Projects\PRTG-Sensors\Upload.bat" "E:\Tools\Upload.bat"
+Copy /y "\\tsclient\D\Projects\droorda\PRTG-Sensors\Upload.bat" "E:\Tools\Upload.bat"
 
 
 
@@ -15,24 +15,24 @@ robocopy "E:\ProgramData\Paessler\PRTG Network Monitor\Configuration Auto-Backup
 
 FOR /F "usebackq" %%v IN (`wmic datafile where name^='C:\\Program Files ^(x86^)\\PRTG Network Monitor\\PRTG Server.exe' get version`) DO call :setPRTGVersion %%v
 
-if Not Exist "\\tsclient\D\Projects\PRTG-Sensors\devicetemplates.OEM\%PRTGVersion%" (
+if Not Exist "\\tsclient\D\Projects\droorda\PRTG-Sensors\devicetemplates.OEM\%PRTGVersion%" (
     Echo "Backing up New Version deviceTemplates %PRTGVersion%"
-    RoboCopy "C:\Program Files (x86)\PRTG Network Monitor\devicetemplates"       "\\tsclient\D\Projects\PRTG-Sensors\devicetemplates.OEM\%PRTGVersion%"           /MIR /NJH /NJS /NFL /W:3
+    RoboCopy "C:\Program Files (x86)\PRTG Network Monitor\devicetemplates"       "\\tsclient\D\Projects\droorda\PRTG-Sensors\devicetemplates.OEM\%PRTGVersion%"           /MIR /NJH /NJS /NFL /W:3
 )
 
 Echo Checking for Upload Changes
-RoboCopy "\\tsclient\D\Projects\PRTG-Sensors\Custom Sensors\EXE"            "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXE"    /MIR /NJH /NJS /NDL /W:3 /l
-RoboCopy "\\tsclient\D\Projects\PRTG-Sensors\Custom Sensors\EXEXML"         "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXEXML" /MIR /NJH /NJS /NDL /W:3 /l
-RoboCopy "\\tsclient\D\Projects\PRTG-Sensors\lookups\custom"                "C:\Program Files (x86)\PRTG Network Monitor\lookups\custom"        /MIR /NJH /NJS /NDL /W:3 /l
-RoboCopy "\\tsclient\D\Projects\PRTG-Sensors\devicetemplates"               "C:\Program Files (x86)\PRTG Network Monitor\devicetemplates"       /MIR /NJH /NJS /NDL /W:3 /l
-RoboCopy "\\tsclient\D\Projects\Scripts\Servers\AMZ-PRTG"                   "E:\Batch"                                                        S*.ps1 /NJH /NJS /NDL /W:3 /l /XX
+RoboCopy "\\tsclient\D\Projects\droorda\PRTG-Sensors\Custom Sensors\EXE"            "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXE"    /MIR /NJH /NJS /NDL /W:3 /l
+RoboCopy "\\tsclient\D\Projects\droorda\PRTG-Sensors\Custom Sensors\EXEXML"         "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXEXML" /MIR /NJH /NJS /NDL /W:3 /l
+RoboCopy "\\tsclient\D\Projects\droorda\PRTG-Sensors\lookups\custom"                "C:\Program Files (x86)\PRTG Network Monitor\lookups\custom"        /MIR /NJH /NJS /NDL /W:3 /l
+RoboCopy "\\tsclient\D\Projects\droorda\PRTG-Sensors\devicetemplates"               "C:\Program Files (x86)\PRTG Network Monitor\devicetemplates"       /MIR /NJH /NJS /NDL /W:3 /l
+RoboCopy "\\tsclient\D\Projects\droorda\Scripts\Servers\AMZ-PRTG"                   "E:\Batch"                                                        S*.ps1 /NJH /NJS /NDL /W:3 /l /XX
 pause
 
-RoboCopy "\\tsclient\D\Projects\PRTG-Sensors\Custom Sensors\EXE"            "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXE"    /MIR /NJH /NJS /NDL /W:3 /J
-RoboCopy "\\tsclient\D\Projects\PRTG-Sensors\Custom Sensors\EXEXML"         "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXEXML" /MIR /NJH /NJS /NDL /W:3 /J
-RoboCopy "\\tsclient\D\Projects\PRTG-Sensors\lookups\custom"                "C:\Program Files (x86)\PRTG Network Monitor\lookups\custom"        /MIR /NJH /NJS /NDL /W:3 /J
-RoboCopy "\\tsclient\D\Projects\PRTG-Sensors\devicetemplates"               "C:\Program Files (x86)\PRTG Network Monitor\devicetemplates"       /MIR /NJH /NJS /NDL /W:3 /J
-RoboCopy "\\tsclient\D\Projects\Scripts\Servers\AMZ-PRTG"                   "E:\Batch"                                                        S*.ps1 /NJH /NJS /NDL /W:3 /J /XX
+RoboCopy "\\tsclient\D\Projects\droorda\PRTG-Sensors\Custom Sensors\EXE"            "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXE"    /MIR /NJH /NJS /NDL /W:3 /J
+RoboCopy "\\tsclient\D\Projects\droorda\PRTG-Sensors\Custom Sensors\EXEXML"         "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXEXML" /MIR /NJH /NJS /NDL /W:3 /J
+RoboCopy "\\tsclient\D\Projects\droorda\PRTG-Sensors\lookups\custom"                "C:\Program Files (x86)\PRTG Network Monitor\lookups\custom"        /MIR /NJH /NJS /NDL /W:3 /J
+RoboCopy "\\tsclient\D\Projects\droorda\PRTG-Sensors\devicetemplates"               "C:\Program Files (x86)\PRTG Network Monitor\devicetemplates"       /MIR /NJH /NJS /NDL /W:3 /J
+RoboCopy "\\tsclient\D\Projects\droorda\Scripts\Servers\AMZ-PRTG"                   "E:\Batch"                                                        S*.ps1 /NJH /NJS /NDL /W:3 /J /XX
 
 
 GOTO:EOF
